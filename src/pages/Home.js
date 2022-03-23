d21c0c076688383b4f4a240e2ec9f4d0578913a3
Sararid <sarawen88@gmail.com>
2022-03-23 18:43:59 +0100

    add template redux-toolkit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../redux/features/counter/counterSlice'
 export default function Home() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
     return (
-        <div>Home</div>
+        <div>Home
+            <button onClick={() => dispatch(increment())}>
+                increment
+            </button>
+            <span>{count}</span>
+        </div>
     )
 }
